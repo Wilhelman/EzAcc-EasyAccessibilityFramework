181b65fc99deac417124246811b4ea946301687a
Wilhelman <guillermogarsub@gmail.com>
2020-04-05 20:37:56 +0200

    New debug script to Log stuff

---
+++ EzAcc/EzAcc/EzAcc_Log.h
@@ -0,0 +1,37 @@
+/*
+  EzAcc - Easy Accessibility Framework
+  Copyright (C) 2020 Guillermo García Subirana <guillermogarsub@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+
+#ifndef _EZACC_LOG_H_
+#define _EZACC_LOG_H_
+
+#include <windows.h>
+#include <stdio.h>
+
+#include "EzAcc_Defines.h"
+
+#define LOG(format, ...) log(__FILE__, __LINE__, format, __VA_ARGS__)
+
+void log(const char file[], int line, const char* format, ...);
+
+#endif
